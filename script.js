248332b9250349921b283700b9a517fb98de73f6
aarshpatel1 <aarsh18patel@gmail.com>
2024-10-08 15:24:42 +0530

    api works

--- script.js
+++ script.js
@@ -16,7 +16,7 @@
 // })
 
 
-// // method - 2 
+// // method - 2
 // document.getElementById("fetch-img").addEventListener("click", function () {
 //     let promise = fetch("https://dog.ceo/api/breeds/image/random")
 //     promise.then((response) => {
@@ -26,3 +26,25 @@
 //         console.log(imgData)
 //     })
 // })
+
+
+document.getElementById("fetch-img").addEventListener("click", function () {
+
+    let promise = fetch("https://dog.ceo/api/breeds/image/random")
+
+    promise.then((response) => {
+        console.log(response.status)
+        return response.json()
+    }).then((imgData) => {
+        console.log(imgData)
+        const imgContainer = document.getElementById("img-container")
+        imgContainer.classList.remove("d-none")
+        const dogImage = document.createElement("img")
+        dogImage.src = imgData.message
+        imgContainer.appendChild(dogImage)
+    }).catch((error) => {
+        console.log(error)
+    })
+
+    this.style.display = "none"
+})
