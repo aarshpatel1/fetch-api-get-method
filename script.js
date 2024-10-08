a68a6f3a28da95b004305118faeb8281a38cef08
aarshpatel1 <aarsh18patel@gmail.com>
2024-10-08 15:11:50 +0530

    2 methods add of fetching dog image data from api

---
+++ script.js
@@ -0,0 +1,28 @@
+// // method - 1
+// document.getElementById("fetch-img").addEventListener("click", function () {
+//     let xhr = new XMLHttpRequest()
+//     xhr.open("GET", "https://dog.ceo/api/breeds/image/random", true)
+//     xhr.onload = function () {
+//         console.log(this.status)
+//         if (this.status === 200 || this.status === 201) {
+//             console.log("Image Fetched Successfully")
+//             let response = JSON.parse(this.responseText)
+//             console.log(response)
+//         } else {
+//             console.log("Error! Image could not fetched.")
+//         }
+//     }
+//     xhr.send()
+// })
+
+
+// // method - 2 
+// document.getElementById("fetch-img").addEventListener("click", function () {
+//     let promise = fetch("https://dog.ceo/api/breeds/image/random")
+//     promise.then((response) => {
+//         console.log(response.status)
+//         return response.json()
+//     }).then((imgData) => {
+//         console.log(imgData)
+//     })
+// })
